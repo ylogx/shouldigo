d41eb852aa29bef88851ebfea48095138413c471
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-27 13:17:04 +0530

    Created weathercheck view and fixed urls

--- weathercheck/models.py
+++ weathercheck/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Weather(models.Model):
+    temprature = models.IntegerField(default=0)
+    timestamp = models.DateTimeField('Timestamp', default=timezone.now())
+    wind = models.IntegerField(default=0)
+
+    #def __init__(self, temprature, timestamp, wind=0):
+        #self.temprature = temprature
+        #self.timestamp = timestamp
+        #self.wind = wind
+
+    def __str__(self):
+        output = ''
+        output += 'Temprature: ' + str(self.temprature) + ' '
+        output += 'Timestamp: ' + str(self.timestamp) + ' '
+        output += 'Wind: ' + str(self.wind) + ' '
+        return output
