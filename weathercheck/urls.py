995fa0d974142baaebf2844e651af067f3067d80
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-27 15:11:24 +0530

    Added weather check api, location and ip

--- weathercheck/urls.py
+++ weathercheck/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from weathercheck import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.today, name='today'),
+    #url(r'^$', views.index, name='index'),
     url(r'today/', views.today, name='today'),
 )
--- weathercheck/views.py
+++ weathercheck/views.py
@@ -11,26 +11,51 @@ def index(request):
 def today(request):
     ''' Main view for today's weather
     '''
-    return HttpResponse('Your location is %s and your ip is %s'%(
-                            get_location(), get_ip()))
+    ip = get_ip(request)
+    location = get_location(ip)
+    weather = weather_check(location)
+    out = 'Your location is %s and your ip is %s'%(location, ip)
+    out += '</br>'
+    out += 'Weather around you is: ' + weather
+    return HttpResponse(out)
 
 
-def get_location():
-    ''' Get the current location of user via ip-api
+def request_json(url):
+    ''' Send a urllib request for json item
     '''
-    ip = get_ip()
-    url = 'http://ip-api.com/json/' + ip
     req = urllib.request.Request(url)
     json_bytes = urllib.request.urlopen(req).read()
     json_str = json_bytes.decode()
     json_dict = json.loads(json_str)
+    return json_dict
+
+def weather_check(location):
+    api_key = 'af28f357f23b0ae779a75617eec26398'
+    #TODO: Use country in location below
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location
+    json_dict = request_json(url)
+    output = json_dict['weather']
+    output = output[0]['main']
+    return output
+
+def get_location(ip):
+    ''' Get the current location of user via ip-api
+    '''
+    if ip == '127.0.0.1':
+        return 'Delhi'  #FIXME: Use some other location
+    url = 'http://ip-api.com/json/' + ip
+    json_dict = request_json(url)
     city = ''   #FIXME: Fatal, need backup method
     if 'city' in json_dict.keys():
         city = json_dict['city']
     return city
 
-def get_ip():
+def get_ip(request):
     ''' Get the IP address of user
     '''
-    ip = '14.140.107.130'   #TODO
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
     return ip
