71edc39335638d68baf1343e3fc924cf707e09c6
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-06 00:51:31 +0530

    Add google site verification file via html page
    Expose site verification html using urls file

--- shouldigo/settings.py
+++ shouldigo/settings.py
@@ -86,4 +86,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, 'staticfiles'),
+        )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
--- shouldigo/urls.py
+++ shouldigo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^weathercheck/', include('weathercheck.urls')),
     url(r'^$', include('weathercheck.urls')),   #XXX: Points to weathercheck
+    url(r'^googleeac30150b558f9bc\.html$', RedirectView.as_view(url='/static/googleeac30150b558f9bc.html'))
 )
